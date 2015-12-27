3379cf1c0b57bf26dce0830bd608a354e56b9a4e
Aaron Sarazan <aaron@levelmoney.com>
2015-12-27 15:42:31 -0800

    re-add wire functions

---
+++ src/main/java/com/levelmoney/bismarck4/serializers/WireSerializer.kt
@@ -0,0 +1,24 @@
+package com.levelmoney.bismarck4.serializers
+
+import com.levelmoney.bismarck.plugins.Serializer
+import com.levelmoney.bismarck4.Serializer
+import com.squareup.wire.Message
+import com.squareup.wire.Wire
+import java.io.*
+
+/**
+ * Created by Aaron Sarazan on 9/25/14
+ * Copyright(c) 2014 Level, Inc.
+ */
+public class WireSerializer<R : Message>(val cls: Class<R>) : Serializer<R> {
+
+    override fun writeObject(stream: OutputStream, data: R): Boolean {
+        stream.write(data.toByteArray())
+        return true
+    }
+
+    override fun readObject(stream: InputStream): R? {
+        return Wire().parseFrom(stream, cls)
+    }
+
+}
