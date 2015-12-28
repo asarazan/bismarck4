0c87fbe8b55942c321e615eb7baaa804ec047fd6
Aaron Sarazan <aaron@levelmoney.com>
2015-12-28 11:59:17 -0800

    remove old test

--- src/test/java/com/levelmoney/bismarck4/DependencyTests.kt
+++ src/test/java/com/levelmoney/bismarck4/DependencyTests.kt
@@ -22,19 +22,4 @@ class DependencyTests {
         assertEquals(1, count1)
         assertEquals(1, count2)
     }
-
-    @Test
-    fun testDedupeCascade() {
-        var count1 = 0
-        var count2 = 0
-        val b1 = Bismarcks.dedupingBismarck<Int>().fetcher { ++count1 }
-        val b2 = Bismarcks.dedupingBismarck<Int>().fetcher { ++count2 }
-        b1.addDependent(b2)
-        b1.invalidate()
-        Thread.sleep(1L)
-        b1.invalidate()
-        Thread.sleep(1L)
-        assertEquals(2, count1)
-        assertEquals(2, count2)
-    }
 }
